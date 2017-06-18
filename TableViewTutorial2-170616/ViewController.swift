bf36c1718775442b1d31ee5e29c7eca2a0ce6115
Joachim Vetter <JoachimVetter@joachims-mbp>
2017-06-18 19:37:51 +0200

    Search Bar added

--- TableViewTutorial2-170616/ViewController.swift
+++ TableViewTutorial2-170616/ViewController.swift
@@ -8,22 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
+    @IBOutlet weak var mySearchbar: UISearchBar!
     @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         myTableView.delegate = self
         myTableView.dataSource = self
+        mySearchbar.delegate = self
         myContent.fillBool()
+        print(myContent.filteredItems)
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myContent.puzzles.count
+        return myContent.filteredItems.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let myCurrent = myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! myPrototypeCell
-        myCurrent.myLabel.text = myContent.puzzles[indexPath.row]
-        myCurrent.myImages.image = UIImage(named: myContent.bilder[indexPath.row])
+        myCurrent.myLabel.text = myContent.filteredItems[indexPath.row]
+        myCurrent.myImages.image = UIImage(named: myContent.filteredItems[indexPath.row])
         return myCurrent
     }
     
@@ -40,7 +43,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             myContent.bilder.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
         }
-        myTableView.reloadData()
+        //myTableView.reloadData()
         print(myContent.puzzles, myContent.bilder)
     }
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
@@ -66,6 +69,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             myContent.colorsOfRows[path.row] = !myContent.colorsOfRows[path.row]
         }
         myTableView.reloadData()
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if let text = searchBar.text {
+            let eingabe = text.trimmingCharacters(in: .whitespaces)
+            myContent.filterData(search: eingabe)
+            print(myContent.filteredItems)
+            myTableView.reloadData()
+        }
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
--- TableViewTutorial2-170616/myData.swift
+++ TableViewTutorial2-170616/myData.swift
@@ -12,14 +12,30 @@ class myData {
     var puzzles = [String]()
     var bilder = [String]()
     var colorsOfRows = [Bool]()
+    var filteredItems:[String] = []
     init() {
         puzzles = ["Sudoku", "Kakuro", "Hakyuu", "Nurikabe", "Battleships", "Skyscrapers"]
         bilder = ["book1", "book2", "book3", "book4", "book5", "book6"]
+        filterData(search: "")
     }
     func fillBool() {
         for _ in 0..<puzzles.count {
             colorsOfRows.append(false)
         }
+    }
+    func filterData(search: String) {
+        if search == "" {
+            filteredItems = puzzles
+        } else {
+            filteredItems = puzzles.filter({
+                (item) in
+                let value1 = item.lowercased()
+                let value2 = search.lowercased()
+                let valid = value1.hasPrefix(value2)
+                return valid
+            })
+        }
+        filteredItems.sort(by: {(value1, value2) in value1 < value2})
     }
 }
 var myContent = myData()
